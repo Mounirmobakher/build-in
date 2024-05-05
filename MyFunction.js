04ea334ac8442e13bcfd970d67d9e8375238471f
Mounirmobakher <mounirkh1284@gmail.com>
2024-05-05 23:41:55 +0300

    Add files via upload

---
+++ MyFunction.js
@@ -0,0 +1,295 @@
+document.getElementById('Ta1').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat1');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('Ta1').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date1');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta2').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat2');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta2').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date2');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta3').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat3');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta3').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date3');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta4').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat4');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta4').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date4');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta5').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat5');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta5').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date5');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta6').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat6');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta6').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date6');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta7').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat7');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta7').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date7');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta8').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat8');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta8').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date8');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta9').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat9');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta9').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date9');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta10').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat10');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta10').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date10');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta11').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat11');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta11').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date11');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta12').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat12');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta12').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date12');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });document.getElementById('ta13').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('dat13');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  document.getElementById('ta13').addEventListener('change', function() {
+    var isChecked = this.checked;
+    var secondRow = document.getElementById('date13');
+  
+    if (isChecked) {
+
+      secondRow.style.display = '';
+    } else {
+      secondRow.style.display = 'none';
+    }
+  });
+  var selectButtons = document.querySelectorAll('.selectProperty');
+
+selectButtons.forEach(function(radio) {
+  radio.addEventListener('click', function() {
+    var propertyRow = this.parentNode.parentNode;
+    var propertyName = propertyRow.querySelector('td:first-child').textContent;
+    var propertyPrice = propertyRow.querySelector('td:nth-child(3)').textContent;
+    document.getElementById('propertyForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert("تم إرسال البيانات بنجاح!\n\nالعقار المختار: " + propertyName + "\nالسعر: " + propertyPrice);
+    });
+  });
+});
+var selectButtons = document.querySelectorAll('.btn-next');
+selectButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      document.getElementById('formContainer').style.display = 'block';
+    });
+    });
